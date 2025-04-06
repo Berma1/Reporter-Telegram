e08f827ee42629ba2b76487061a063180f95698e
Berma1 <piwan0890@gmail.com>
2025-04-06 00:51:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -63,4 +63,5 @@ if number == "1":
 if number == "2":
 	os.system("python report/report.py")
 elif number == "3":
-    print ("This section is being updated and will be added soon \n\nChannel : @esfelurm")
+    print ("This section is being updated and will be added soon \n\nChannel :@SAHIMPATUKIYA")
+	
